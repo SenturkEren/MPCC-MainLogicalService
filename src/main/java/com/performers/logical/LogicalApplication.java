bae5e5c795e503573f10d1ce1cf9fcd460ef1203
eren.senturk@vodafone.com <eren.senturk@vodafone.com>
2021-11-19 23:41:29 +0300

    initial

---
+++ src/main/java/com/performers/logical/LogicalApplication.java
@@ -0,0 +1,13 @@
+package com.performers.logical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/performers/logical/LogicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.performers.logical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
